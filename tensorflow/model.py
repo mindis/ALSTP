6801c94f91b67b35e6203c66ccecd7555dad2e2b
yangyangguo <35999950+guoyang9@users.noreply.github.com>
2018-05-19 16:52:50 +0800

    Update model.py

--- tensorflow/model.py
+++ tensorflow/model.py
@@ -35,8 +35,8 @@ class ALSTP(object):
 		self.is_training = is_training
 
 		self.lr = tf.get_variable(name='lr', shape=[], 
-							trainable=False,
-							initializer=tf.constant_initializer(lr))
+						trainable=False,
+						initializer=tf.constant_initializer(lr))
 
 		self.local_dim = int(0.4 * global_dim)
 		self.batch_size = 1 #Without parallism
@@ -44,7 +44,7 @@ class ALSTP(object):
 	def inference(self):
 		""" Initialize important settings. """
 		self.regularizer = tf.contrib.layers.l2_regularizer(
-													self.regularizer_rate)
+							self.regularizer_rate)
 
 		if self.activation_func == 'ReLU':
 			self.activation_func = tf.nn.relu 
@@ -55,10 +55,10 @@ class ALSTP(object):
 
 		if self.optim == 'SGD':
 			self.optim = tf.train.GradientDescentOptimizer(
-													self.lr, name='SGD')
+							self.lr, name='SGD')
 		elif self.optim == 'Momentum':
 			self.optim = tf.train.MomentumOptimizer(self.lr, 
-											momentum=0.9, name='RMSProp')
+							momentum=0.9, name='RMSProp')
 		elif self.optim == 'Adam':
 			self.optim = tf.train.AdamOptimizer(self.lr, name='Adam')
 
@@ -71,63 +71,63 @@ class ALSTP(object):
 
 		with tf.name_scope("input"):
 		    self.item_pre = tf.placeholder(dtype=tf.float32,
-		    						name='item_pre',
-		    						shape=[self.num_steps, self.fix_dim])
+		    					name='item_pre',
+		    					shape=[self.num_steps, self.fix_dim])
 		    self.item_target = tf.placeholder(dtype=tf.float32,
-		    						name='item_target',
-		    						shape=[1, self.fix_dim])
+		    					name='item_target',
+		    					shape=[1, self.fix_dim])
 		    self.query_pre =  tf.placeholder(dtype=tf.float32,
-		    						name='query_pre',
-		    						shape=[self.num_steps, self.fix_dim])
+		    					name='query_pre',
+		    					shape=[self.num_steps, self.fix_dim])
 		    self.query_target =  tf.placeholder(dtype=tf.float32,
-		    						name='query_target',
-		    						shape=[1, self.fix_dim])
+		    					name='query_target',
+		    					shape=[1, self.fix_dim])
 		    self.neg_samples = tf.placeholder(dtype=tf.float32,
-		    						name='neg_samples',
-		    						shape=[self.num_negative, self.fix_dim])
+		    					name='neg_samples',
+		    					shape=[self.num_negative, self.fix_dim])
 		    self.all_items = tf.placeholder(dtype=tf.float32,
-		    						name='all_items',
-		    						shape=[self.item_num, self.fix_dim])
+		    					name='all_items',
+		    					shape=[self.item_num, self.fix_dim])
 
 	def create_model(self):
 		""" Create model from scratch. """
 		with tf.name_scope("convert"):
 			self.item_convert_pre = tf.layers.dense(self.item_pre, 
-									units=self.global_dim,
-									activation=self.activation_func,
-									kernel_regularizer=self.regularizer,
-									name='convert_matrix',
-									reuse=tf.AUTO_REUSE)
+							units=self.global_dim,
+							activation=self.activation_func,
+							kernel_regularizer=self.regularizer,
+							name='convert_matrix',
+							reuse=tf.AUTO_REUSE)
 			self.item_convert_target = tf.layers.dense(self.item_target, 
-									units=self.global_dim,
-									activation=self.activation_func,
-									kernel_regularizer=self.regularizer,
-									name='convert_matrix',
-									reuse=tf.AUTO_REUSE)
+							units=self.global_dim,
+							activation=self.activation_func,
+							kernel_regularizer=self.regularizer,
+							name='convert_matrix',
+							reuse=tf.AUTO_REUSE)
 			self.query_convert_pre = tf.layers.dense(self.query_pre, 
-									units=self.global_dim,
-									activation=self.activation_func,
-									kernel_regularizer=self.regularizer,
-									name='convert_matrix',
-									reuse=tf.AUTO_REUSE)
+							units=self.global_dim,
+							activation=self.activation_func,
+							kernel_regularizer=self.regularizer,
+							name='convert_matrix',
+							reuse=tf.AUTO_REUSE)
 			self.query_convert_target = tf.layers.dense(self.query_target, 
-									units=self.global_dim,
-									activation=self.activation_func,
-									kernel_regularizer=self.regularizer,
-									name='convert_matrix',
-									reuse=tf.AUTO_REUSE)
+							units=self.global_dim,
+							activation=self.activation_func,
+							kernel_regularizer=self.regularizer,
+							name='convert_matrix',
+							reuse=tf.AUTO_REUSE)
 			self.neg_convert_samples = tf.layers.dense(self.neg_samples, 
-									units=self.global_dim,
-									activation=self.activation_func,
-									kernel_regularizer=self.regularizer,
-									name='convert_matrix',
-									reuse=tf.AUTO_REUSE)
+							units=self.global_dim,
+							activation=self.activation_func,
+							kernel_regularizer=self.regularizer,
+							name='convert_matrix',
+							reuse=tf.AUTO_REUSE)
 			self.all_convert_items = tf.layers.dense(self.all_items, 
-									units=self.global_dim,
-									activation=self.activation_func,
-									kernel_regularizer=self.regularizer,
-									name='convert_matrix',
-									reuse=tf.AUTO_REUSE)
+							units=self.global_dim,
+							activation=self.activation_func,
+							kernel_regularizer=self.regularizer,
+							name='convert_matrix',
+							reuse=tf.AUTO_REUSE)
 
 		with tf.name_scope("local_context"):
 			self.initial_state = tf.transpose(
@@ -138,71 +138,71 @@ class ALSTP(object):
 							initial_state=self.initial_state,
 							dtype=tf.float32)
 			self.outputs_reshape = tf.reshape(self.outputs, 
-										[self.num_steps, self.global_dim])
+							[self.num_steps, self.global_dim])
 
 			# Compute local attention weights for items.
 			self.queries_local = tf.layers.dense(self.query_convert_pre,
-									units=self.local_dim,
-									activation=None,
-									kernel_regularizer=self.regularizer,
-									name='queries_local')
+							units=self.local_dim,
+							activation=None,
+							kernel_regularizer=self.regularizer,
+							name='queries_local')
 			self.query_local = tf.layers.dense(self.query_convert_target,
-									units=self.local_dim,
-									activation=None,
-									kernel_regularizer=self.regularizer,
-									name='query_local')
+							units=self.local_dim,
+							activation=None,
+							kernel_regularizer=self.regularizer,
+							name='query_local')
 			self.v_local = tf.layers.dense(
-									self.activation_func(
-									self.query_local+self.queries_local),
-									units=1,
-									activation=None,
-									kernel_regularizer=self.regularizer,
-									name='v_local')
+							self.activation_func(
+							self.query_local+self.queries_local),
+							units=1,
+							activation=None,
+							kernel_regularizer=self.regularizer,
+							name='v_local')
 			assert self.v_local.shape == [self.num_steps, 1]
 
 			self.local_weights = tf.nn.softmax(self.v_local, axis=0,
-													name='local_weights')
+							name='local_weights')
 
 			self.local_context = tf.reduce_sum(tf.multiply(
-								self.outputs_reshape, self.local_weights),
-								axis=0, keepdims=True, name='local_context')
+							self.outputs_reshape, self.local_weights),
+							axis=0, keepdims=True, name='local_context')
 
 		with tf.name_scope("global_context"):
 			# Compute global attention weights for global interest.
 			self.query_global = tf.layers.dense(self.query_target,
-									units=1,
-									activation=self.activation_func,
-									kernel_regularizer=self.regularizer,
-									name='query_global')
+							units=1,
+							activation=self.activation_func,
+							kernel_regularizer=self.regularizer,
+							name='query_global')
 			self.global_new = tf.matmul(self.global_interest, 
-									self.query_global, name='global_new')
+							self.query_global, name='global_new')
 			self.global_weights = tf.nn.softmax(self.global_new, axis=0,
-													name='global_weights')
+							name='global_weights')
 
 			self.global_context = tf.transpose(tf.multiply(
-												self.global_interest, 
-												self.global_weights),
-												name='global_context')
+							self.global_interest, 
+							self.global_weights),
+							name='global_context')
 
 		with tf.name_scope("concatenation"):
 			self.concate = tf.concat([self.local_context, self.global_context,
-										self.query_convert_target], axis=1)
+							self.query_convert_target], axis=1)
 
 			self.query_integration = tf.layers.dense(self.concate,
-										units=self.global_dim,
-										activation=self.activation_func,
-										kernel_regularizer=self.regularizer,
-										name='query_integration')
+							units=self.global_dim,
+							activation=self.activation_func,
+							kernel_regularizer=self.regularizer,
+							name='query_integration')
 
 		with tf.name_scope("similarity"):
 			self.target_item = tf.nn.l2_normalize(
-											self.item_convert_target, 1)
+							self.item_convert_target, 1)
 			self.samples_neg = tf.nn.l2_normalize(
-											self.neg_convert_samples, 1)
+							self.neg_convert_samples, 1)
 			self.query_integration = tf.nn.l2_normalize(
-											self.query_integration, 1)
+							self.query_integration, 1)
 			self.items_all = tf.nn.l2_normalize(
-											self.all_convert_items, 1)
+							self.all_convert_items, 1)
 
 			self.query_integration_negs = tf.concat(
 						[self.query_integration for _ in 
@@ -224,12 +224,12 @@ class ALSTP(object):
 		with tf.name_scope("loss"):
 			reg = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
 			self.reg_loss = tf.contrib.layers.apply_regularization(
-												self.regularizer, reg)
+							self.regularizer, reg)
 
 			self.pos_score = tf.reshape(self.pos_score, [-1])
 			self.neg_scores = tf.reshape(self.neg_scores, [-1])
 			self.loss = -tf.reduce_mean(tf.log(tf.sigmoid(
-							self.pos_score-self.neg_scores))) + self.reg_loss
+						self.pos_score-self.neg_scores))) + self.reg_loss
 
 		with tf.name_scope("optimizer"):
 			tvars = tf.trainable_variables()
@@ -268,11 +268,11 @@ class ALSTP(object):
 
 	def cons_global_interest(self, sess, old_state):
 		sess.run(self.global_interest_con, feed_dict={
-									self.old_state: old_state})
+							self.old_state: old_state})
 
 	def update_global_interest(self, sess, old_state):
 		sess.run(self.global_interest_update, feed_dict={
-									self.old_state: old_state})
+							self.old_state: old_state})
 
 	def assign_global_interest(self, sess, global_interest):
 		sess.run(self.global_interest_assign, feed_dict={
@@ -288,7 +288,7 @@ class ALSTP(object):
 	def summary(self):
 		""" Create summaries to write on tensorboard. """
 		self.writer = tf.summary.FileWriter('./graphs/ALSTP', 
-												tf.get_default_graph())
+								tf.get_default_graph())
 		with tf.name_scope("summaries"):
 			tf.summary.scalar('loss', self.loss)
 			tf.summary.histogram('histogram loss', self.loss)
